c43261a6bc442fa3d65fe2601a2fd485920563de
ozgur çimen <derozgur@gmail.com>
2014-11-05 17:14:48 +0200

    ++

--- www/androidsystem.js
+++ www/androidsystem.js
@@ -11,31 +11,6 @@ AndroidSystem.alert = function(){
 	exec(null,null,"AndroidSystem","alert",["deneme"]);
 }
 
-/*
-Keyboard.hideKeyboardAccessoryBar = function(hide) {
-    exec(null, null, "Keyboard", "hideKeyboardAccessoryBar", [hide]);
-};
-
-Keyboard.close = function() {
- exec(null, null, "Keyboard", "close", []);
-};
-
-Keyboard.show = function() {
- exec(null, null, "Keyboard", "show", []);
-};
-
-Keyboard.disableScroll = function(disable) {
- exec(null, null, "Keyboard", "disableScroll", [disable]);
-};
-
-/*
-Keyboard.styleDark = function(dark) {
- exec(null, null, "Keyboard", "styleDark", [dark]);
-};
-*/
-*/
-
-
 module.exports = AndroidSystem;
 
 
